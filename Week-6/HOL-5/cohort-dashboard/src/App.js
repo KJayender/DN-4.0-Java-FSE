482caca45a1a2f9f94366b8df5a883b40ff25500
KJayender <148576575+KJayender@users.noreply.github.com>
2025-08-02 10:47:57 +0530

    Add files via upload

---
+++ Week-6/HOL-5/cohort-dashboard/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CohortDetails from './components/CohortDetails';
+
+function App() {
+  return (
+    <div>
+      <CohortDetails name="Batch 1" status="ongoing" />
+    </div>
+  );
+}
+
+export default App;
