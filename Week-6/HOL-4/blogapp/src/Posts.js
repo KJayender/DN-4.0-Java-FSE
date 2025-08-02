58a849b1a317a0a0bce2d1aa3d947621d3d2c3da
KJayender <148576575+KJayender@users.noreply.github.com>
2025-08-02 10:44:12 +0530

    Add files via upload

---
+++ Week-6/HOL-4/blogapp/src/Posts.js
@@ -0,0 +1,49 @@
+// src/Posts.js
+import React, { Component } from 'react';
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      error: null
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ posts: data }))
+      .catch(error => this.setState({ error }));
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert('An error occurred: ' + error);
+  }
+
+  render() {
+    const { posts, error } = this.state;
+
+    if (error) {
+      return <div>Error loading posts.</div>;
+    }
+
+    return (
+      <div>
+        <h1>Posts</h1>
+        {posts.map(post => (
+          <div key={post.id} style={{ marginBottom: '20px' }}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
